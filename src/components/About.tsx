300cf08faf4cf0abb7c3e69350e81623dc94d23c
Jesus-fhz <jesusflores9325@gmail.com>
2022-05-21 19:10:18 +1000

    changin about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 
-import '../styles/about.scss'
+import "../styles/about.scss";
 
 const About: React.FC = () => {
-
-    return( 
-        <section className="about main-setting" id="about">
+    return (
+        <section className='about main-setting' id='about'>
             <span>
-                <h1 className="title-panels"> About me</h1>
+                <h1 className='title-panels'> About me</h1>
             </span>
-            <div className="panels">
-                <p>  
-                    I'm an <span>IT Engineer</span>  and a recent <span>Software Engineer Graduate from GA</span>   with two years of international commercial experience in web development as a full-stack developer. 
+            <div className='panels'>
+                <p>
+                    I have a bachelor’s degree in <span>Computer Science</span> and a recent{" "}
+                    <span>Software Engineer Graduate from GA</span> with two years of international
+                    commercial experience in web development as a full-stack developer.
                 </p>
                 <p>
-                I am looking forward to working with an amazing team in a great environment, where I can use my unique skill-set as a web developer and expand my knowledge but also continuously add value to the lives of those around me.
+                    I am looking forward to working with an amazing team in a great environment,
+                    where I can use my unique skill-set as a web developer and expand my knowledge
+                    but also continuously add value to the lives of those around me.
                 </p>
             </div>
         </section>
-    )
+    );
+};
 
-}
-
-export default About
+export default About;
